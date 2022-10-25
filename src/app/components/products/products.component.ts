60b0fe2588e612f421f6644e3704994cb40ef940
LMerino420 <merinolev96@gmail.com>
2022-10-25 12:52:55 -0600

    Product service http

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { ProductsService } from '../../services/product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(private productService: ProductsService) {}
 
   ngOnInit(): void {
+    this.getAllProducts();
   }
 
+  getAllProducts() {
+    this.productService.getAllSimple().subscribe((prod) => {
+      this.products = prod;
+      console.log('PROD =>', this.products);
+    });
+  }
 }
