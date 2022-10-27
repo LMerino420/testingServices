f6fef47bc15bc20fd341e581c8f70d99f5fa8cad
LMerino420 <merinolev96@gmail.com>
2022-10-26 18:22:11 -0600

    Github Actions config

--- karma.conf.js
+++ karma.conf.js
@@ -36,6 +36,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ["Chrome"],
+    customLaunchers: {
+      ChromeCI: {
+        base: "Chrome",
+        flags: ["--no-sandbox"],
+      },
+    },
     singleRun: false,
     restartOnFileChange: true,
   });
