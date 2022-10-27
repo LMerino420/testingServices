a34773969d7d6bb4aaa7e1999eaddee03d267d69
LMerino420 <merinolev96@gmail.com>
2022-10-27 11:55:28 -0600

    Test Component @Input

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -1,6 +1,7 @@
 import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { Person } from 'src/app/models/person.model';
 
 import { PersonComponent } from './person.component';
 
@@ -24,17 +25,33 @@ fdescribe('PersonComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Deberia tener un <h3>Hola, PersonComponent</h3>', () => {
+  it('Deberia mostrar un nombre {{person?.name}}', () => {
+    component.person = new Person('Lev', 'Merino', 26, 40, 1.85);
+    expect(component.person.name).toEqual('Lev');
+  });
+
+  it('Deberia tener un <h3>Hola, {{person?.name}}</h3>', () => {
+    //Arrange
+    component.person = new Person('Andrei', 'Merino', 26, 40, 1.85);
+    const expectMsg = `Hola, ${component.person.name}`;
     const personDebug: DebugElement = fixture.debugElement;
     const h3Debug: DebugElement = personDebug.query(By.css('h3'));
     const h3Element: HTMLElement = h3Debug.nativeElement;
-    expect(h3Element?.textContent).toEqual('Hola, PersonComponent');
+    //Act
+    fixture.detectChanges();
+    //Assert
+    expect(h3Element?.textContent).toContain(expectMsg);
   });
 
-  it('Deberia tener un <p>Este es un parrafo</p>', () => {
+  it('Deberia tener un <p>Mi altura es {{person?.age}}</p>', () => {
+    //Arrange
+    component.person = new Person('Andrei', 'Merino', 26, 40, 1.85);
     const personDebug: DebugElement = fixture.debugElement;
     const pDebug: DebugElement = personDebug.query(By.css('p'));
     const pElement: HTMLElement = pDebug.nativeElement;
-    expect(pElement?.textContent).toEqual('Este es un parrafo');
+    //Act
+    fixture.detectChanges();
+    //Assert
+    expect(pElement?.textContent).toContain(component.person.height);
   });
 });
